7c1f4d760a8db281ac4837cdaae461e8dbef7f87
Polyhistor <pouya.ataei.7@gmail.com>
2020-09-03 20:16:26 +1200

    created admin route and controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('components.homemaster');
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/admin', 'AdminsController@index')->name('admin.index');
