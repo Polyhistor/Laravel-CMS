1bea3b282cbbbfcdb4b31ff5776b9aec587591fb
Polyhistor <pouya.ataei.7@gmail.com>
2020-09-05 20:09:47 +1200

    added data table

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
 
-    public function show()
+    public function index()
     {
-        return view('blog-post');
+        return view('admin.posts.index');
+    }
+
+    public function show(Post $post)
+    {
+        return view('blog-post', ['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $inputs = request()->validate([
+            'title' => 'required|min:8|max:255',
+            'post_image' => 'file',
+            'body' => 'required'
+        ]);
+
+        if (request('post_image')) {
+            $inputs['post_image'] = request('post_image')->store('images');
+        }
+
+        auth()->user()->posts()->create($inputs);
+
+        return back();
     }
 }
